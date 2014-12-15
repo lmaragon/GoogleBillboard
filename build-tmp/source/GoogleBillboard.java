61cadbcd33612cbf3cc850752c8eb22e4f432692
unknown <luaragon@697-334-Rohan.sf.k12.internal>
2014-12-15 12:18:01 -0800

    Added first name to index.html

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -15,17 +15,54 @@ import java.io.IOException;
 public class GoogleBillboard extends PApplet {
 
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+double nums;
+
 public void setup()  
 {            
-     noLoop();  
+     noLoop(); 
+
+     
+     // for (int i = 0; i < nList.length(); i++)
+     // {
+     // 	nList = e.substring(i,i+10);
+     // 	nums = Double.parseDouble(nList);
+     // 	if (isPrime(nums))
+     // 	{
+     // 		System.out.println(nums);
+     // 	}
+     // }
+     for (int i = 2; i < e.length() - 10; i++)
+     {
+		String digits = e.substring(i,i+10);
+		double dNum = Double.parseDouble(digits);
+		if (isPrime(dNum) == true)
+		{
+			System.out.println(isPrime(dNum));
+			System.out.println(digits);
+			break;
+		}
+     }
+     	
 }  
+
 public void draw()  
 {   
-	//not needed for this assignment
+	
+	
+	//String nList = new String();
+	//not needed for this assignment	
 }  
 public boolean isPrime(double dNum)  
 {   
-    //to be finished later   
+    //to be finished later 
+    for (int i=2; i <= Math.sqrt(dNum); i++)
+    {
+    	if(dNum % i == 0)
+    	{
+      		return false;
+    	}
+    }  
+    //nums = dNum;
     return true;  
 } 
   static public void main(String[] passedArgs) {
